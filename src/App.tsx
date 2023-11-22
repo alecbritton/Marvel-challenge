445a242e1d1fb08d0f2de9a5ea73fa5181d361cd
Alec Britton <alec.britton1@outlook.com>
2023-11-22 20:40:13 +0000

    initial functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
+import "./App.css";
+interface MarvelCharacter {
+  id: number;
+  name: string;
+}
+interface MarvelResponse {
+  data: {
+    results: MarvelCharacter[];
+  };
+}
 
 function App() {
+  const [characters, setCharacters] = useState<MarvelCharacter[]>([]);
+  const [input, setInput] = useState<string>("");
+
+  const MARVEL_URL = "https://gateway.marvel.com:443/v1/public";
+  const MARVEL_API_KEY = "15c480e9c4dbce6a4d5424def1269e05";
+
+  const fetchMarvelData = async (searchQuery: string) => {
+    try {
+      const marvelData = await axios.get<MarvelResponse>(
+        `${MARVEL_URL}/characters?nameStartsWith=${searchQuery}&apikey=${MARVEL_API_KEY}`
+      );
+
+      return marvelData.data;
+    } catch (error) {
+      console.error(error);
+      return { data: { results: [] } };
+    }
+  };
+
+  const onUserChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInput(value);
+    if (value.length > 1) {
+      const suggestions = await fetchMarvelData(value);
+
+      const { results } = suggestions && suggestions.data;
+
+      if (results) setCharacters(results);
+      else setCharacters([]);
+    } else {
+      setCharacters([]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type="text"
+          placeholder="Search for a character..."
+          value={input}
+          onChange={onUserChange}
+        />
+        {characters.length > 0 && (
+          <div>
+            {characters.map((character) => {
+              return (
+                <div
+                  className="suggestion-item"
+                  key={character.id}
+                  onClick={(e) => {
+                    setInput("");
+                    setCharacters([]);
+                    alert(character.name);
+                  }}
+                >
+                  {character.name}
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
